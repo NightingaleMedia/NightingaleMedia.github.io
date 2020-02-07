0c3129c0a3c3f9db1ff67444cdc10278d2d1c3a6
NightingaleMedia.AlbertMacbook <alsigman@gmail.com>
2020-02-07 18:25:30 -0500

    added the skills scrum board and added styling for it.  finished the javascript functionality for the instagram. added the header star function

---
+++ assets/js/charts.js
@@ -0,0 +1,8 @@
+var ctx = document.getElementById('myChart').getContext('2d');
+var myBubbleChart = new Chart(ctx, {
+    type: 'bubble',
+    data: {[
+      one{[1,2,3]}
+    ]}
+    options:
+});
