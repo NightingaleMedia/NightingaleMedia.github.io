64539a7f658d2f9ca10a9d25463ec6ec7fab2499
NightingaleMedia.AlbertMacbook <alsigman@gmail.com>
2020-06-28 21:27:10 -0400

    added the correct google sheet integration

--- assets/js/scrum.js
+++ assets/js/scrum.js
@@ -1,21 +1,79 @@
+const boards = document.querySelectorAll('.skill-scrum--board');
+const skillSquare = document.querySelectorAll('.skill-square')
 
-    const handleDrop = (ev, elem) => {
-        ev.preventDefault();
-        event.target.classList.remove('opaque')
-        const data = ev.dataTransfer.getData('text/plain');
-        elem.appendChild(document.getElementById(data))
 
+
+function buildSkills(skills){
+    
+
+    function findBoardFromSkill(skill){
+        const board = document.querySelector(`[data-mastery=${skill.mastery}]`)
+        return board;
     }
+    skills.forEach(skill =>{
+            let skillNode = document.createElement('div');
+            skillNode.className = `single-skill ${skill.cat.toLowerCase()}`
+            skillNode.innerHTML = ` 
+              <div class="single-skill--content">${skill.skill}</div>
+              <div class="single-skill--drag">
+                <span></span>
+                <span></span>
+                <span></span>
+              </div>`
+
+    let boardToFill = findBoardFromSkill(skill);
+    boardToFill.appendChild(skillNode)
+
+ 
+    })
+    matchSkills();
+    initDraggers();
+
+}
+
+    function matchSkills() {
+    const allSkills = document.querySelectorAll('.single-skill')
+        skillSquare.forEach(square => {
+            let correspondingSkills = [];
+            allSkills.forEach(skill => {
+            
+                if (square.classList[1] == skill.classList[1])
+                    correspondingSkills.push(skill)
+
+            })
+            square.addEventListener('click', () => {
+                correspondingSkills.forEach(skill => {
+                    skill.classList.toggle('single-skill--off')
+                })
+                square.classList.toggle('skill-square--off');
+            })
+        })
+    }
+const parseSkills = async () => {
+    let url = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vSbtgf0y6rmYI-lg4tCbelwoK0IshswVbW_F519K6xXpk-HYJo71c3ShxoQcZ-5B8fZHDhF9_h_AtwA/pub?output=csv&gid=1872270793';
+    return await d3.dsv(',', url, function (d) {
+        return {
+            'skill': d.Skill,
+            'mastery': d.Mastery,
+            'cat': d.Category,
+        };
+    }).then(result => result)
+
+}
+
+const handleDrop = (ev, elem) => {
+    ev.preventDefault();
+    event.target.classList.remove('opaque')
+    const data = ev.dataTransfer.getData('text/plain');
+    elem.appendChild(document.getElementById(data))
+}
 
 const handleDragOver = (ev, elem) => {
-    // document.style.cursor = "grabbing";
     ev.preventDefault();
     ev.dataTransfer.dropEffect = "move";
 }
 
 const handleDrag = (ev, elem) => {
-
-
     const elemWidth = elem.offsetWidth;
     ed = ev.dataTransfer;
 
@@ -24,12 +82,12 @@ const handleDrag = (ev, elem) => {
     ed.setDragImage(elem, `${elemWidth - 20}`, 20);
     ed.effectAllowed = "move";
     ed.dropEffect = "move";
-    console.log(ed)
+
 }
 
-const boards = document.querySelectorAll('.skill-scrum--board');
-boards.forEach(board => {
 
+
+boards.forEach(board => {
     board.ondrop = (event) => {
         handleDrop(event, board)
         board.classList.remove('opaque')
@@ -45,8 +103,11 @@ boards.forEach(board => {
     }
 })
 
+
+function initDraggers(){ 
 const draggers = document.querySelectorAll('.single-skill--drag');
 draggers.forEach(sticky => {
+    
     let fullSticky = sticky.parentElement
     fullSticky.id = `draggy-${sticky.previousElementSibling.innerText}`
     sticky.parentElement.ondrop = (e) => {
@@ -61,24 +122,13 @@ draggers.forEach(sticky => {
         sticky.parentElement.classList.remove('dragging')
     })
 })
+} 
 
-const skillSquare = document.querySelectorAll('.skill-square')
-const allSkills = document.querySelectorAll('.single-skill')
 
-skillSquare.forEach(square => {
-        let correspondingSkills = [];
-        allSkills.forEach(skill => {
-            if (square.classList[1] == skill.classList[1])
-                correspondingSkills.push(skill)
-        })
+parseSkills()
+    .then(ans => buildSkills(ans))
 
-        square.addEventListener('click', () => {
-            correspondingSkills.forEach(skill => {
-                skill.classList.toggle('single-skill--off')
-                // square.addEventListener('transitionend',()=>{
-                //     skill.classList.toggle('d-none')
-                // })
-            })
-            square.classList.toggle('skill-square--off');
-        })
-    }) 
+
+   
+
+
